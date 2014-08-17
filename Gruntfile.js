24e603961381138286cd506d879c092619a3b1cb
Jordan Buckley <jordan@bkly.org>
2014-08-17 13:53:15 -0700

    Add Browserify task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,19 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js', 'index.js', 'server.js']
+      all: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js']
+    },
+    browserify: {
+      './dist/bundle.js' : [ './app/app.js']
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['browserify']);
 };
 
 
